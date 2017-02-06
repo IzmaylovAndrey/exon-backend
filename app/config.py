eb5a548c979d63a7d506fb1da9755eac238515a9
IzmaylovAndrey <whiteshaq41@gmail.com>
2017-02-06 13:45:47 +0300

    Clean config to temp version

--- app/config.py
+++ app/config.py
@@ -1,13 +1,12 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/exon'
 
-MAIL_USERNAME = 'email@example.com'
-MAIL_PASSWORD = 'password'
-MAIL_DEFAULT_SENDER = '“Sender” <noreply@example.com>’'
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USE_TLS = False
+# TODO: Config mailing
+# MAIL_USERNAME = 'email@example.com'
+# MAIL_PASSWORD = 'password'
+# MAIL_DEFAULT_SENDER = '“Sender” <noreply@example.com>’'
+# MAIL_SERVER = 'smtp.gmail.com'
+# MAIL_PORT = 465
+# MAIL_USE_SSL = True
+# MAIL_USE_TLS = False
 
