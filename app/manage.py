ac678ddb9ce68ca3b03c558bddcbaaa7ea2259b0
IzmaylovAndrey <whiteshaq41@gmail.com>
2017-02-06 16:34:59 +0300

    Create migration script

---
+++ app/manage.py
@@ -0,0 +1,13 @@
+from app import app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
+
+app.config.from_object('config')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
