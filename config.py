3e00d3920ec1269736c4ef8cedb6024ee40ae8ca
IzmaylovAndrey <whiteshaq41@gmail.com>
2017-02-07 18:38:18 +0300

    F-king magic part 1

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ FLASK_DEBUG = True
 # TODO: Config mailing
 # MAIL_USERNAME = 'email@example.com'
 # MAIL_PASSWORD = 'password'
-# MAIL_DEFAULT_SENDER = '“Sender” <noreply@example.com>’'
+# MAIL_DEFAULT_SENDER = 'Sender <noreply@example.com>'
 # MAIL_SERVER = 'smtp.gmail.com'
 # MAIL_PORT = 465
 # MAIL_USE_SSL = True
